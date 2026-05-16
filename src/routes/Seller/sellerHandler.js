8874e7e564bae0d180bfe42fc256d42b64699d7c
Daniel_12 <danielvillarraga52@gmail.com>
2026-05-15 22:16:28 -0500

    primeraSeller

--- src/routes/Seller/sellerHandler.js
+++ src/routes/Seller/sellerHandler.js
@@ -1,6 +1,11 @@
 
-const askAllSellers=(req,res)=>{
-
+const askAllSellers=async(req,res)=>{
+    try {
+        const allSeller=await getAllSeller();
+        res.status(200).json(allSeller);
+    } catch (error) {
+        res.status(400).json({error:error.message});
+    }
 };
 const askSeller=(req,res)=>{
 
